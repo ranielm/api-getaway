4dabe859f9e46161f5cb0e4c3b39dc7f18eaf02b
Raniel Mendonça <raniel.mendonca@luizalabs.com>
2023-10-20 14:15:20 -0300

    instalando redis

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,75 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import axios from "axios";
 import dotenv from "dotenv";
+import redis from "redis";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.GATEWAY_PORT || 4000;
+const REDIS_PORT = 6379;
+console.log("🚀 ~ file: app.ts:11 ~ REDIS_PORT:", REDIS_PORT);
 
-const EVENTS_SERVICE_URL = process.env.EVENTS_SERVICE_URL;
-const USERS_SERVICE_URL = process.env.USERS_SERVICE_URL;
+const client = redis.createClient({
+  url: `redis://localhost:${REDIS_PORT}`,
+});
+
+client.on("error", (error: any) => console.error(`Error : ${error}`));
 
 app.use(express.json());
 
-app.use("/events", async (req, res) => {
-  try {
-    const url = `${EVENTS_SERVICE_URL}${req.url}events`;
-    const method = req.method;
-    const data = req.body;
+async function fetchApiData(id: string) {
+  console.log("🚀 ~ file: app.ts:22 ~ fetchApiData ~ id:", id);
+  const apiResponse = await axios.get(`http://localhost:4000/events/${id}`);
+  console.log("Request sent to the API");
+  console.log(
+    "🚀 ~ file: app.ts:24 ~ fetchApiData ~ apiResponse:",
+    apiResponse
+  );
+  return apiResponse.data;
+}
 
-    const response = await axios({
-      method,
-      url,
-      data,
-    });
-    res.status(response.status).send(response.data);
-  } catch (error: any) {
-    if (axios.isAxiosError(error)) {
-      res.status(error.response?.status || 500).send(error.response?.data);
+const cacheMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const key = req.params.event;
+  try {
+    const cacheResults = await client.get(key);
+    if (cacheResults) {
+      res.send({
+        fromCache: true,
+        data: JSON.parse(cacheResults),
+      });
     } else {
-      res.status(500).send({ message: "An unexpected error occurred" });
+      next();
     }
+  } catch (error) {
+    console.error(error);
+    next();
   }
-});
+};
 
-// Encaminhar solicitações para o microsserviço de usuários
-app.use("/users", async (req, res) => {
+app.get("/events/:id", cacheMiddleware, async (req: Request, res: Response) => {
+  const id = req.params.id;
+  console.log("🚀 ~ file: app.ts:56 ~ app.use ~ id:", id);
   try {
-    const response = await axios({
-      method: req.method,
-      url: `${USERS_SERVICE_URL}${req.url}`,
-      data: req.body,
-    });
-    res.status(response.status).send(response.data);
-  } catch (error: any) {
-    if (axios.isAxiosError(error)) {
-      res.status(error.response?.status || 500).send(error.response?.data);
-    } else {
-      res.status(500).send({ message: "An unexpected error occurred" });
+    const results = await fetchApiData(id);
+    if (results.length === 0) {
+      throw "API returned an empty array";
     }
+    await client.set(id, JSON.stringify(results));
+    res.send({
+      fromCache: false,
+      data: results,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).send("Data unavailable");
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`API Gateway is running on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
