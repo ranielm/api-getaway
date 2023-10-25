f4801914eca56f89675ef4e0dfe7c2db960c35f3
Raniel Mendonça <raniel.mendonca@luizalabs.com>
2023-10-25 18:00:55 -0300

    iniciando redis

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 // Importação dos módulos necessários
-import express from "express";
+import express, { NextFunction, Response, Request } from "express";
 import axios from "axios";
 import dotenv from "dotenv";
+import redis from "redis";
+import { promisify } from "util";
 
 // Carrega as variáveis de ambiente do arquivo .env
 dotenv.config();
@@ -18,12 +20,37 @@ const USERS_SERVICE_URL =
 const EVENTS_SERVICE_URL =
   process.env.EVENTS_SERVICE_URL || "http://localhost:3002";
 
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+// Inicialização do cliente Redis
+const client = redis.createClient({
+  url: `redis://localhost:${REDIS_PORT}`,
+});
+
+client.on("error", (error: any) => console.log(`Error: ${error}`));
+
 // Middleware para parsear o corpo da requisição em formato JSON
 app.use(express.json());
+
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.setex).bind(client);
 
 const headers = {
   Origin: "http://localhost:4000",
 };
+
+const cacheMiddleware =
+  (key: string) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const cacheResults = await getAsync(key);
+      if (cacheResults) {
+        res.send({
+          fromCache: true,
+          data: JSON.parse(cacheResults),
+        });
+      }
+    } catch (error) {}
+  };
 
 // Rota para lidar com as solicitações relacionadas a eventos
 app.use("/events", async (req, res) => {
@@ -76,11 +103,9 @@ app.use("/users", async (req, res) => {
     if (axios.isAxiosError(error)) {
       res.status(error.response?.status || 500).send(error.response?.data);
     } else {
-      res
-        .status(500)
-        .send({
-          message: "Não foi possível acessar o microsserviços de usuários",
-        });
+      res.status(500).send({
+        message: "Não foi possível acessar o microsserviços de usuários",
+      });
     }
   }
 });
